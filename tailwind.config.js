c9ce582441a584e4345d049def20c427d688534c
yongaricode <kang061313@gmail.com>
2024-06-26 21:31:31 +0900

    TailwindCSS & material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{js,jsx}'],
+	content: [
+		'./src/**/*.{js,jsx}',
+		'./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./node_modules/@material-tailwind/react/theme/components**/*.{js,ts,jsx,tsx,mdx}',
+	],
 	theme: {
 		extend: {},
 	},
