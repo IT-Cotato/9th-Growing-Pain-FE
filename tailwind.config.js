340c83c46a2971cc4a34cec43d1be6ef34c9fbe2
yongaricode <kang061313@gmail.com>
2024-06-27 17:54:05 +0900

    Common Component: Button

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,19 @@ export default {
 		extend: {
 			colors: {
 				gray: {
+					// Footer - 구분선
 					light: '#D1D1D1',
+					// Footer - 글자들
 					dark: '#6F6F6F',
+				},
+				navy: {
+					// 로그인 & 회원가입 버튼 색
+					dark: '#26408B',
+					// 자소서 관리 시작 버튼 색
+					light: '#5D5A88',
 				},
 			},
 		},
+		plugins: [],
 	},
-	plugins: [],
 };
