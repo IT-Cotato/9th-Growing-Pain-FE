9c19be478c12cdc6a9b061e2a1b78267ee340d7a
yongaricode <kang061313@gmail.com>
2024-08-01 20:41:15 +0900

    [UPDATE] 커뮤니티 태그 수정, 댓글창 모달 url 설정

--- src/components/CategoryTag.jsx
+++ src/components/CategoryTag.jsx
@@ -11,11 +11,12 @@ const CategoryTag = ({ category }) => {
 
 	return (
 		<div className="flex gap-[11px]">
-			{(category === 'project' || category === 'study' || category === 'contest') && (
-				<div className="w-[100px] h-[37px] rounded-[10px] bg-gray-line text-center py-[7px] text-[15px] font-normal">
-					팀원모집
-				</div>
-			)}
+			{location.pathname === '/user/community/total' &&
+				(category === 'project' || category === 'study' || category === 'contest') && (
+					<div className="w-[100px] h-[37px] rounded-[10px] bg-gray-line text-center py-[7px] text-[15px] font-normal">
+						팀원모집
+					</div>
+				)}
 			<div className="w-[100px] h-[37px] rounded-[10px] bg-gray-line text-center py-[7px] text-[15px] font-normal">
 				{categoryName}
 			</div>
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,9 @@
-import { FaRegCommentAlt, FaRegHeart, FaHeart, FaBookmark, FaRegBookmark } from 'react-icons/fa';
-import { useState } from 'react';
+import { FaRegCommentAlt, FaRegHeart, FaRegBookmark } from 'react-icons/fa';
+import { useAtom } from 'jotai';
 import CommentModal from '../components/CommentModal';
 import CategoryTag from './CategoryTag';
+import { useLocation } from 'react-router-dom';
+import { modalOpenAtom } from '../utils/atoms';
 
 const PostItem = ({
 	id,
@@ -17,15 +19,10 @@ const PostItem = ({
 	bookmark,
 	replies,
 }) => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
+	const location = useLocation();
+	const [modalId, setmodalId] = useAtom(modalOpenAtom);
 
-	const ModalOpen = () => {
-		setIsModalOpen(true);
-	};
-
-	const ModalClose = () => {
-		setIsModalOpen(false);
-	};
+	const isModalOpen = modalId === id;
 
 	return (
 		<div className="bg-white mr-[36px] rounded-[10px]">
@@ -52,39 +49,47 @@ const PostItem = ({
 							<FaRegHeart className="w-[20px] h-[20px]" />
 							{heart}
 						</span>
-						<span className="flex items-center gap-[10px] text-[14px] font-medium cursor-pointer" onClick={ModalOpen}>
+						<span
+							className="flex items-center gap-[10px] text-[14px] font-medium cursor-pointer"
+							onClick={() => setmodalId(id)}
+						>
 							<FaRegCommentAlt className="w-[20px] h-[20px]" />
 							{comments.length}
 						</span>
-						<span className="flex  cursor-pointer">
+						<span className="flex cursor-pointer">
 							<FaRegBookmark className="w-[20px] h-[20px]" />
 						</span>
 					</div>
 				</div>
-				{/* 글 본문, 카테고리 태그*/}
+				{/* 글 본문, 카테고리 태그 */}
 				<div className="flex justify-between gap-[36px]">
 					<div className="flex flex-col gap-4 mt-[36px] rounded-[10px] place-items-start">
 						<h1 className="text-[18px] font-medium">{postTitle}</h1>
 						<h1 className="flex text-[16px] leading-[31px] whitespace-pre-line text-left">{content}</h1>
 					</div>
+
 					<div className="self-end">
-						<CategoryTag category={Array.isArray(category) ? category[1] : category} />
+						{(location.pathname === '/user/community/total' || location.pathname === '/user/community/member') && (
+							<CategoryTag category={Array.isArray(category) ? category[1] : category} />
+						)}
 					</div>
 				</div>
-				<CommentModal
-					isOpen={isModalOpen}
-					onClose={ModalClose}
-					id={id}
-					nickname={nickname}
-					createdTime={createdTime}
-					position={position}
-					postTitle={postTitle}
-					content={content}
-					userProfile={userProfile}
-					heart={heart}
-					comments={comments}
-					replies={replies}
-				/>
+				{isModalOpen && (
+					<CommentModal
+						isOpen={modalId}
+						onClose={() => window.history.back()}
+						id={id}
+						nickname={nickname}
+						createdTime={createdTime}
+						position={position}
+						postTitle={postTitle}
+						content={content}
+						userProfile={userProfile}
+						heart={heart}
+						comments={comments}
+						replies={replies}
+					/>
+				)}
 			</div>
 		</div>
 	);
