9a665ee3736ab96d31dad7914465063c26e111b6
EunsolnCho <cho_eunsol@naver.com>
2024-08-03 21:37:00 +0900

    [FEAT] 마이페이지 알림 페이지 UI

---
+++ src/components/NotificationItem.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from '../assets/images/logo.png';
+import { useNavigate } from 'react-router-dom';
+import calculateTimeAgo from '../utils/calculateTimeAgo';
+
+const NotificationItem = ({ id, profile, nickname, time, content, type }) => {
+  const nav = useNavigate();
+
+  if(type==='community'){
+    return (
+      <div className='notification-item mb-[20px] rounded-[10px] border-b-[1px] border-gray-300'>
+        <div className='flex flex-col'>
+          <div className='flex items-center gap-[7px]'>
+            <img src={profile} alt='Profile' className='w-[30px] h-[30px] rounded-full' />
+            <div className='text-[14px] font-medium'>{nickname}</div>
+            <div className='text-[14px] text-gray-400'>•</div>
+            <div className='text-[11px] text-gray-400'>{calculateTimeAgo(time)}</div>
+          </div>
+          <p className='my-[15px] text-[13px] text-left'>{content}</p>
+        </div>
+      </div>
+    );
+  }
+  else{
+    return (
+      <div className='notification-item mb-[20px] rounded-[10px] border-b-[1px] border-gray-300'>
+        <div className='flex flex-col'>
+          <div className='flex items-center gap-[7px]'>
+            <img src={logo} alt='Profile' className='w-[30px] h-[30px] rounded-full' />
+            <div className='text-[14px] font-medium'>성장통</div>
+          </div>
+          <div className='flex items-center justify-between'>
+            <p className='my-[15px] text-[13px] text-left'>{content}</p>
+            <div className='my-[15px] text-[13px] text-center text-navy-dark cursor-pointer' onClick={()=>nav(`/user/growth/apply/${id}`)}>확인하기</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default NotificationItem;
