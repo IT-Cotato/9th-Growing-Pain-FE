9d6b176e025cd256ed3a8dea033f231f5dbec958
EunsolnCho <cho_eunsol@naver.com>
2024-08-12 18:58:39 +0900

    [FEAT] 지원현황 생성, 수정 기능

--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -1,5 +1,6 @@
 import EditApply from "../components/EditApply";
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 // 초기 데이터
 const initialData = [
@@ -9,6 +10,7 @@ const initialData = [
     memberId: "greatsounds613@gmail.com",
     jobApplications: [
       {
+        id: 0,
         applicationType: "DOCUMENT",
         place: "",
         result: "PENDING",
@@ -19,6 +21,7 @@ const initialData = [
         jobPostId: 0,
         applicationDetails: [
           {
+            id: 0,
             title: "",
             content: ""
           }
@@ -31,9 +34,26 @@ const initialData = [
 const Record = () => {
   const [data, setData] = useState(initialData);
 
-  // 지원 내역 데이터를 병합하여 저장하는 함수
-  const handleSave = (savedData) => {
-    console.log('저장된 데이터:', savedData);
+  // 데이터 생성
+  const handleSave = async (savedData) => {
+    try {
+    // axios를 사용하여 POST 요청을 보냄
+    const response = await axios.post(
+      'https://5ecc59c9-4083-4c5b-9271-8a9fca225f08.mock.pstmn.io/api/job-posts/', 
+      savedData, // 전송할 데이터
+      {
+        headers: {
+          'Content-Type': 'application/json', // JSON 형식으로 데이터를 전송
+        },
+      }
+    );
+
+    // 서버 응답을 처리
+    console.log('서버 응답:', response.data);
+  } catch (error) {
+    // 에러 처리
+    console.error('데이터 전송 중 오류 발생:', error);
+  }
   };
 
   // 렌더링
