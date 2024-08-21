7911592b5ec22e0700b423d2353bab31fbe70506
EunsolnCho <cho_eunsol@naver.com>
2024-08-22 02:59:41 +0900

    [FEAT] 메인 홈 구현 및 지원현황 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,40 +2,73 @@ import logo from '../assets/images/logo.png';
 import Button from './Button';
 import { useNavigate } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({type}) => {
 	const nav = useNavigate();
 
-	return (
-		// bg-slate-200: 헤더 확인을 위해 임시로 색상 설정 (06.28)
-		<div className="header-component h-[90px] flex w-full border-b border-[#C7C7C7]">
-			{/* 헤더 로고 및 서비스 이름 -> 클릭하면 홈으로 라우팅 */}
-			<div
-				className="header-logo p-5 flex relative w-1/6 text-[20px] gap-1 items-center cursor-pointer text-navy-dark"
-				onClick={() => nav('/')}
-			>
-				<img className="header-logo-img w-10 h-10" src={logo} alt="Logo" />
-				성장통
-			</div>
-			{/* 헤더 메뉴바 */}
-			<div className="haeder-menu flex relative items-center gap-5 w-3/6">
-				{/* 메뉴바는 버튼이 아니라 해당 부분 클릭하면 라우팅 -> div로 설정 */}
-				<div className="cursor-pointer" onClick={() => nav('/about')}>
-					성장통 소개
+	if (type==="main"){
+		return (
+			<div className="header-component h-[90px] flex w-full">
+				{/* 헤더 로고 및 서비스 이름 -> 클릭하면 홈으로 라우팅 */}
+				<div
+					className="header-logo p-5 flex relative w-[10%] text-[20px] gap-1 items-center cursor-pointer text-navy-dark"
+					onClick={() => nav('/')}
+				>
+					<img className="header-logo-img w-10 h-10" src={logo} alt="Logo" />
+					성장통
 				</div>
-				<div className="cursor-pointer" onClick={() => nav('/user/growth')}>
-					성장기록
+				{/* 헤더 메뉴바 */}
+				<div className="haeder-menu flex relative items-center gap-5 w-[75%]">
+					{/* 메뉴바는 버튼이 아니라 해당 부분 클릭하면 라우팅 -> div로 설정 */}
+					<div className="cursor-pointer" onClick={() => nav('/')}>
+						성장통 소개
+					</div>
+					<div className="cursor-pointer" onClick={() => nav('/user/growth')}>
+						성장기록
+					</div>
+					<div className="cursor-pointer" onClick={() => nav('/user/community')}>
+						커뮤니티
+					</div>
 				</div>
-				<div className="cursor-pointer" onClick={() => nav('/user/community')}>
-					커뮤니티
+				{/* 헤더 버튼 - 로그인, 회원가입 */}
+				<div className="header-button flex relative items-center justify-center gap-[20px] w-[15%]">
+					<Button type="login" text="로그인" onClick={() => nav('/login')} />
+					<Button type="signup" text="회원가입" onClick={() => nav('/signup')} />
 				</div>
 			</div>
-			{/* 헤더 버튼 - 로그인, 회원가입 */}
-			<div className="header-button pr-[10px] flex relative items-center justify-center gap-[20px] w-2/6">
-				<Button type="login" text="로그인" onClick={() => nav('/login')} />
-				<Button type="signup" text="회원가입" onClick={() => nav('/signup')} />
+		);
+	}
+	else {
+		return (
+			<div className="header-component h-[90px] flex w-full border-b border-[#C7C7C7]">
+				{/* 헤더 로고 및 서비스 이름 -> 클릭하면 홈으로 라우팅 */}
+				<div
+					className="header-logo p-5 flex relative w-[10%] text-[20px] gap-1 items-center cursor-pointer text-navy-dark"
+					onClick={() => nav('/')}
+				>
+					<img className="header-logo-img w-10 h-10" src={logo} alt="Logo" />
+					성장통
+				</div>
+				{/* 헤더 메뉴바 */}
+				<div className="haeder-menu flex relative items-center gap-5 w-[75%]">
+					{/* 메뉴바는 버튼이 아니라 해당 부분 클릭하면 라우팅 -> div로 설정 */}
+					<div className="cursor-pointer" onClick={() => nav('/')}>
+						성장통 소개
+					</div>
+					<div className="cursor-pointer" onClick={() => nav('/user/growth')}>
+						성장기록
+					</div>
+					<div className="cursor-pointer" onClick={() => nav('/user/community')}>
+						커뮤니티
+					</div>
+				</div>
+				{/* 헤더 버튼 - 로그인, 회원가입 */}
+				<div className="header-button pr-[10px] flex relative items-center justify-center gap-[20px] w-[15%]">
+					<Button type="login" text="로그인" onClick={() => nav('/login')} />
+					<Button type="signup" text="회원가입" onClick={() => nav('/signup')} />
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default Header;
