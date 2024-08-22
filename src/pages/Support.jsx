8bd7fc1a7308583cd483a7fccdd625cf404070c7
EunsolnCho <cho_eunsol@naver.com>
2024-08-22 15:15:13 +0900

    [UPDATE] App.jsx 목데이터 삭제

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,16 +1,13 @@
-import { GrowthStateContext } from "../App";
-import { useContext } from "react";
 import HeaderMyPage from "../components/HeaderMyPage";
 import MenubarMyPage from "../components/MenubarMyPage";
 
 const Support = () => {
-  const [memberData, jobPostData, applicationData, applicaionDetailData, infoData] = useContext(GrowthStateContext);
 
   return (
     <div>
       <div className="mx-[70px] mt-[53px]">
         <div>
-          <HeaderMyPage name={infoData.name} company={infoData.company} />
+          <HeaderMyPage />
         </div>
         <div className="mypage-content-container flex-col">
           <div className="menubar">
